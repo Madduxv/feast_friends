6d11bf671b0a55f9d5af7191be3e360ba44c166e
Maddux Vitrano <madduxv@Madduxs-MacBook-Air.local>
2023-10-15 10:24:18 -0500

    Initial Commit

---
+++ android/app/src/main/kotlin/com/example/what_to_eat_app/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.what_to_eat_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(new MyApp());
+}
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}): super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(brightness: Brightness.dark),
+      home: Home(),
+    );
+  }
+}
+
+class Home extends StatefulWidget {
+  const Home({Key? key}): super(key: key);
+
+  @override
+  _HomeState createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
+
