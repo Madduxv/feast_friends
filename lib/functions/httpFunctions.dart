9814fd37e938b7a92403f1583cc2d615f89f8d68
Madduxv <madduxvjvitrano@gmail.com>
2024-07-17 19:26:51 -0500

    Semi-functional search page

--- lib/functions/httpFunctions.dart
+++ lib/functions/httpFunctions.dart
@@ -21,7 +21,7 @@ class HttpFunctions {
     // if (!Config().isLoaded) {
     //   throw Exception('Configuration not loaded');
     // }
-    final url = Uri.parse('${Config().baseUrl}/requested_genres');
+    final url = Uri.parse('${Config().baseUrl}/restaurant/requested_genres');
     try {
       final response = await http.get(url);
       if (response.statusCode == 200) {
@@ -38,7 +38,7 @@ class HttpFunctions {
   }
 
   static Future<void> requestGenre(String genre) async {
-    final url = Uri.parse('${Config().baseUrl}/request_genre');
+    final url = Uri.parse('${Config().baseUrl}/restaurant/request_genre');
     try {
       final response = await http.post(url, body: genre);
       if (response.statusCode == 200) {
@@ -52,7 +52,7 @@ class HttpFunctions {
   }
 
   static Future<void> clearRequestedGenres() async {
-    final url = Uri.parse('${Config().baseUrl}/clear_genres');
+    final url = Uri.parse('${Config().baseUrl}/restaurant/clear_genres');
     try {
       final response = await http.post(url);
       if (response.statusCode == 200) {
---
+++ lib/widgets/searchBar.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+
+class MySearchBar extends StatefulWidget {
+  final Function(String) onQueryChanged;
+
+  MySearchBar({required this.onQueryChanged});
+
+  @override
+  _MySearchBarState createState() => _MySearchBarState();
+}
+
+class _MySearchBarState extends State<MySearchBar> {
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: TextField(
+        onChanged: widget.onQueryChanged,
+        decoration: InputDecoration(
+          hintText: 'Search...',
+          border: OutlineInputBorder(
+            borderRadius: BorderRadius.all(Radius.circular(10.0)),
+          ),
+        ),
+      ),
+    );
+  }
+}
+
